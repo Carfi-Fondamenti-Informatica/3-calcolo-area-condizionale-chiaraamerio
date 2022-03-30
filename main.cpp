deac8e18d18d46fa761f2b7ea527f263f9c44548
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-03-30 15:43:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0, s=0;
+   cout<<"inserire due valori reali a e b"<<endl;
+   cin>>a>>b;
+   int opzione;
+   cout<<"inserire un valore intero opzione"<<endl;
+   cin>>opzione;
+   
+   switch(opzione){
+      case 0:
+         s=(a*b)*0,5;
+         cout<<s<<endl;
+         break;
+      case 1:
+         s=a*a;
+         cout<<s<<endl;
+         break;
+      case 2:
+         s=a*b;
+         cout<<s<<endl;
+         break;
+      default:
+         cout<<"opzione non valida"<<endl;
+         
+      }      
    return 0;
 }
